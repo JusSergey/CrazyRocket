fac571c89396ee79f13a74d585fd986374f7dc01
JusSergey <jus.noname@gmail.com>
2016-10-08 15:20:20 +0300

    Load levels

---
+++ Resource/Levels/level_0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tiles" tilewidth="64" tileheight="64" tilecount="1024" columns="32">
+ <image source="Tiles.png" width="2048" height="2048"/>
+</tileset>
---
+++ Resource/Levels/level_7.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="32" tileheight="32" tilecount="4000" columns="50">
+ <image source="tileset.png" width="1601" height="2591"/>
+</tileset>
